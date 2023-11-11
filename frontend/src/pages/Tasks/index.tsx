612d69cbf5ebb94be01cc17a2d3f195f9e8019ea
getulio.souza <getulio.souza@evolutix.com.br>
2023-11-11 14:36:42 -0300

    feature: working-on-table

--- frontend/src/pages/Tasks/index.tsx
+++ frontend/src/pages/Tasks/index.tsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
+import api from "../../services/api";
 
-const Tasks: React.FC = () => {
-  return <h1>Pagina de tarefas</h1>
+interface ITask {
+  id: number;
+  title: string;
+  description: string;
+  finished: boolean;
+  created_at: Date;
+  updated_at: Date;
 }
 
-export default Tasks
+const Tasks: React.FC = () => {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  async function loadTasks() {
+    const response = await api.get("/tasks");
+    console.log(response.data);
+    setTasks(response.data);
+  }
+
+  return (
+    <div className="container">
+      <br />
+      <h1>Página de Tarefas</h1>
+      <br />
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Título</th>
+            <th>Data de atualização</th>
+            <th>Status</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => (
+            <tr key={task.id}>
+              <td>{task.id}</td>
+              <td>{task.title}</td>
+              <td>{task.updated_at}</td>
+              <td>{task.finished ? "Finalizado" : "Pendente"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+export default Tasks;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import 'reflect-metadata';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import routes from './routes';
 import { AppDataSource } from './data-source';
+import routes from './routes';
+import * as cors from "cors"
 
 //comunicacao com o banco de dados
 AppDataSource.initialize().then(async () => {
 
   const app = express();
   
+  app.use(cors());
   app.use(bodyParser.json());
   app.use(routes);
   app.listen(3333);
